e46d9f81dc27be6d37c44f8fdfdb47353b75e8e4
Mackenzie Willborn <mlw60@duke.edu>
2019-04-16 11:37:50 -0400

    initialize MongoDB server to add user_name to DB

---
+++ mongo_server.py
@@ -0,0 +1,27 @@
+from flask_pymongo import PyMongo
+from flask import Flask, request
+from my_class import User
+import datetime
+from pymodm import connect
+
+app = Flask(__name__)
+
+connect("mongodb+srv://mlw60:Wm347609@bme547-r5nv9.mongodb.net/test?retryWrites=true")
+
+app.config['MONGO_DBNAME'] = "bme547"
+app.config['MONGO_URI'] = "mongodb+srv://mlw60:Wm347609@bme547-r5nv9.mongodb.net/test?retryWrites=true"
+
+mongo = PyMongo(app)
+
+def add_user_name(user_name_arg):
+    u = User(user_name=user_name_arg)
+    u.save()
+    
+@app.route("/user_name", methods=["POST"])
+def user_name():
+    r = request.get_json()
+    add_user_name(r)
+    return "Added user name!"
+    
+if __name__ == '__main__':
+    app.run()
