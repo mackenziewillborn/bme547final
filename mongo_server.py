fe93ae6f26aef07ca4a442c3626418232a043a87
Mackenzie Willborn <mlw60@duke.edu>
2019-04-16 19:23:05 -0400

    update to work for username with GUI Client

--- mongo_server.py
+++ mongo_server.py
@@ -3,6 +3,11 @@ from flask import Flask, request
 from my_class import User
 import datetime
 from pymodm import connect
+# import matplotlib
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from skimage import data, img_as_float
+# from skimage import exposure
 
 app = Flask(__name__)
 
@@ -13,15 +18,55 @@ app.config['MONGO_URI'] = "mongodb+srv://mlw60:Wm347609@bme547-r5nv9.mongodb.net
 
 mongo = PyMongo(app)
 
+
+def add_new_user(user_name_arg, processing_type_arg, time_uploaded_arg, time_to_process_arg, image_size_arg):
+    u = User(user_name=user_name_arg,
+             processing_type=processing_type_arg,
+             time_uploaded=time_uploaded_arg,
+             time_to_process=time_to_process_arg,
+             image_size=image_size_arg,
+             )
+    u.save()
+
+
 def add_user_name(user_name_arg):
     u = User(user_name=user_name_arg)
     u.save()
-    
+
+
+# def add_processing_type(user_name_arg, processing_type_arg):
+#     u = User(user_name=user_name_arg,
+#              processing_type=processing_type_arg)
+#     u.save()
+
+
+def upload_time():
+    time_stamp = datetime.datetime.now()
+    return time_stamp
+
+
 @app.route("/user_name", methods=["POST"])
 def user_name():
     r = request.get_json()
     add_user_name(r)
     return "Added user name!"
-    
+
+
+@app.route("/User")
+def add():
+    time_stamp = upload_time
+    add_new_user("katie40", "brighter", time_stamp, "70 seconds", 60)
+    return "Added user!"
+
+
+# @app.route("/Image")
+# def image():
+#     img = data.camera()
+#     plt.figure(figsize=(4, 4))
+#     plt.imshow(img, cmap='gray')
+#     plt.axis("off")
+#     plt.show()
+
+
 if __name__ == '__main__':
     app.run()
